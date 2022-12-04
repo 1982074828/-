e110a027fc98af99d9eda6f77a3de36a3c85ba70
1982074828 <66870351+1982074828@users.noreply.github.com>
2022-12-04 18:34:16 +0800

    Add files via upload

--- getchar.c
+++ getchar.c
@@ -1,12 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h> 
+#include <stdio.h>
 int main()
 {
-	int ch = 0;
-	while((ch = getchar()) != EOF)
-	{	
-		putchar(ch);
-	}
-	return 0;
-
+    char password[20] = {0};
+    int ret = 0;
+    printf("请输入密码-->");
+    scanf("%s",password);//输入密码并存放在password数组中
+    //还剩余一个\n留在缓冲区
+    //读取一下'\n'
+    getchar();//读走\n
+    printf("请确认（Y/N）:");
+    ret = getchar();
+    if(ret == 'Y')
+        printf("确认成功");
+    else
+        printf("已取消");
+        
+    return 0;
 }
